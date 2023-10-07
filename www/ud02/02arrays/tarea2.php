f1fc6250d29aa95d08f804a6854f43abb2e73a87
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-10-08 00:13:21 +0200

    Add Arrays

--- www/ud02/02arrays/tarea2.php
+++ www/ud02/02arrays/tarea2.php
@@ -27,5 +27,13 @@ Necesitamos almacenar la siguiente información en un array multidimensional:
 Almacena e imprime la información. 
 */
 
+$usuarios["John"] = array("email" => "john@demo.com", "website" => "www.john.com", "age" => 22, "password" => "pass");
+$usuarios["Anna"] = array("email" => "anna@demo.com", "website" => "www.anna.com", "age" => 24, "password" => "pass");
+$usuarios["Peter"] = array("email" => "peter@mail.com", "website" => "www.peter.com", "age" => 42, "password" => "pass");
+$usuarios["Max"] = array("email" => "max@mail.com", "website" => "www.max.com", "age" => 33, "password" => "pass");
+
+echo "<pre>";
+print_r($usuarios);
+echo "</pre>";
 
 ?>
--- www/ud02/02arrays/tarea3.php
+++ www/ud02/02arrays/tarea3.php
@@ -5,6 +5,12 @@
 Uso de la función [rand](https://www.php.net/manual/es/function.rand.php). 
 Imprima la matriz creada anteriormente.
 */ 
+for ($i=0; $i < 30; $i++) { 
+    $aleatorios[$i] = rand (0, 20);
+}
+echo "<pre>";
+print_r($aleatorios);
+echo "</pre>";
 
 /* 
 2. (Optativo) Cree una matriz con los siguientes datos: 
@@ -15,10 +21,27 @@ Imprima la matriz creada anteriormente.
     - Ordene los elementos de la matriz e imprima la matriz ordenada. 
     - Agregue los siguientes elementos al comienzo de la matriz: `Apple`, `Melon`, `Watermelon`.
 */
+$protagonistas = ["Batman", "Superman", "Krusty", "Bob", "Mel", "Barney"];
+
+unset($protagonistas[count($protagonistas) - 1]);
+
+echo "La posición de \"superman\" es: ".array_search("Superman", $protagonistas);
+
+array_push($protagonistas, "Carl", "Lenny", "Burns", "Lisa");
+
+sort($protagonistas);
+echo "<pre>";
+print_r($protagonistas);
+echo "</pre>";
+
+array_unshift($protagonistas, "Apple", "Melon", "Watermelon");
+
 
 
 /*3. (Optativo) Cree una copia de la matriz con el nombre `copia` con elementos del 3 al 5.
     - Agregue el elemento 'Pera' al final de la matriz. */ 
+$copia = array_slice($protagonistas, 3, 3);
+array_push($copia, "Pera");
 
 
 ?>
