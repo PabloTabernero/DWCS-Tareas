3252202188b11f256107aa2c525f27a652bcedb2
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-10-20 22:09:40 +0200

    Modificacion tarea1.php de arrays

--- www/ud02/02arrays/tarea1.php
+++ www/ud02/02arrays/tarea1.php
@@ -5,12 +5,13 @@ for ($i=0; $i < 10; $i++) {
     $pares[$i] = ($i + 1) * 2;
 }
 
+echo "<h4>10 Primeros Numeros Pares</h4>";
 foreach ($pares as $par) {
     echo $par."<br />";
 }
 
 /* 2. Imprime los valores del array asociativo siguiente usando un foreach:*/
-echo "<br />";
+echo "<h4>Valores del array asociativo</h4>";
 $v[1]=90;
 $v[10] = 200;
 $v['hola']=43;
