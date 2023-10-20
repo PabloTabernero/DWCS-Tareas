65fe7e2c4e7f8251f68d6ef2bd1bbae11d7ad944
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-10-20 20:36:51 +0200

    Modificacion tarea1 de introduccion

--- www/ud02/01introduccion/tarea1.php
+++ www/ud02/01introduccion/tarea1.php
@@ -3,12 +3,13 @@
 Localiza y corrige los errores de este programa PHP:
 ```php **/
 
-echo '¿Cómo estás';
+echo '¿Cómo estás?<br />';
 echo 'Estoy bien, gracias.';
 
 /*Los errores que he encontrado serían:
-    La existencia de 2 etiquetas "<?php", una dentro de la otra.
-    Y que sobraba un signo de "?" en la etiqueta de cierre. **/
+    - La existencia de 2 etiquetas "<?php", una dentro de la otra.
+    - Sobra un signo de "?" en la etiqueta de cierre. 
+    - Muestra el mensaje junto sin espacios, se incorpora un salto de linea.**/
 ?>
 
 
