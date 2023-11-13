992ad85ffd49c23f0a8c2f4ecaf268517cdca5dd
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-11-13 23:32:04 +0100

    Realizado listar.php

--- www/ud03/tienda/listar.php
+++ www/ud03/tienda/listar.php
@@ -7,6 +7,12 @@
     <title>Tienda IES San Clemente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        td,th {
+            padding: 5px;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,15 +20,27 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
-    <p>Lista de usuarios con enlaces para borrar y editar</p>
+
+    <!--Lista de usuarios con enlaces para borrar y editar-->
     <?php
-        //Obter conexión
-        //Seleccionar bd
+        include ("lib/base_datos.php");
+        $conexion = get_conexion();
+        seleccionar_bd_tienda($conexion);
         //Consulta obtención dos usuarios (array)
-        //Crear lista de usuarios
-        //  - cada usuario mostra dous enlaces (editar e borrar)
-        //  - editar.php?id=4
-        //  - borrar.php?id=7
+        $sql = "SELECT id, nombre, apellidos, edad, provincia FROM usuarios";
+        $resultados = $conexion->query($sql);
+        if ($resultados->num_rows > 0) {
+            echo "<table><tr><th>ID</th><th>Nombre</th><th>Apellidos</th><th>Edad</th><th>Provincia</th></tr>";
+            while($row = $resultados->fetch_assoc()){
+                echo "<tr><td>".$row["id"]."</td><td>".$row["nombre"]."</td><td>".$row["apellidos"]."</td><td>".$row["edad"]."</td><td>".$row["provincia"]."</td>
+                <td><a class=\"btn btn-primary\" href=\"editar.php?id=".$row["id"]." role=\"button\"> Editar</a></td>
+                <td><a class=\"btn btn-primary\" href=\"borrar.php?id=".$row["id"]." role=\"button\"> Borrar</a></td>
+                </tr>";
+            }
+            echo "</table>";
+        }else{
+            echo "0 resultados.";
+        }
     ?>
     <footer>
         <p>
