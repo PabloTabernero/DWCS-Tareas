c65a425ede2cda02226a086c90354be33be82b3b
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-11-13 21:53:28 +0100

    Realizadas Funciones BD para index

--- www/ud03/tienda/lib/base_datos.php
+++ www/ud03/tienda/lib/base_datos.php
@@ -1,13 +1,43 @@
 <?php
 
+//Función que realiza la conexión a la BD y devuelve el objeto conexion.
 function get_conexion(){
-    //Código para establecer á conexión
-    //return $conexion;
+    $conexion = new mysqli('db', 'root', 'test');
+    $error = $conexion->connect_errno;
+    if ($error != null) {
+        die("Fallo en la conexión: ".$conexion->connect_error." con numero ".$error."<br />");
+    }
+    return $conexion;
 }
 
+//Función que selecciona la bd tienda, deveulve true o false.
 function seleccionar_bd_tienda($conexion){
-    $conexion->select_db("tienda");
+    return $conexion->select_db("tienda");
+};
+
+function crear_bd_tienda($conexion){
+    $sql = "CREATE DATABASE tienda";
+    return $conexion->query($sql);
 }
-function crear_bd_tienda(){
-    //Código para creación de bd
+
+//Función que comprueba si existe la tabla de usuarios, devuelve true o false.
+function comprobar_tabla_usuarios($conexion){
+    $sql = "SHOW TABLES LIKE 'usuarios'";
+    $result = $conexion->query($sql);
+    return $result->num_rows > 0;
 }
+
+//Función que crea la tabla de usuarios.
+function crear_tabla_usuarios($conexion){
+    $sql ="CREATE TABLE usuarios(
+        id INT AUTO_INCREMENT PRIMARY KEY, 
+        nombre VARCHAR(50),
+        apellidos VARCHAR(100),
+        edad INT,
+        provincia VARCHAR(50)
+    )";
+
+    return $conexion->query($sql);
+}
+    
+    
